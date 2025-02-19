36e2044a1fffeb3ac1b341b45a31778a6bb3bac4
AlbertoMameli <mamelialberto2@gmail.com>
2025-02-19 23:25:55 +0100

    feat: stampo solo FizzBuzz

--- js/script.js
+++ js/script.js
@@ -24,20 +24,19 @@ Consigli del giorno:
 
 for (let i = 1; i <= 100; i++) {
     
-    
     let FizzBuzz;
     if (i % 3 === 0 && i % 5 === 0) {
         FizzBuzz = "FizzBuzz";
+        console.log (FizzBuzz);
     } else if (i % 5 === 0){
         FizzBuzz = "Buzz";
+        console.log (FizzBuzz);
     }
     else if (i % 3 === 0) {
-        FizzBuzz = "Fizz";
+        FizzBuzz = "Fizz"; 
+        console.log (FizzBuzz);
     }
-     
-         
     
-       
-    
-    console.log (i, FizzBuzz);
+
 }
+
